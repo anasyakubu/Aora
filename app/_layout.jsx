8a25df90716391a6871db159cf1721ca673bea41
anasyakubu <yakubuanas04@gmail.com>
2024-07-07 12:15:29 +0100

    SlashScreen and Login Screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -38,8 +38,10 @@ const RootLayout = () => {
   return (
     <>
       <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="profile" />
+        {/* <Stack.Screen name="search/[query]" options={{ headerShown: false }} /> */}
       </Stack>
     </>
   );
